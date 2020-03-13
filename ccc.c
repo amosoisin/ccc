1a7938d67bd20ceff210d62256d022b168681ea1
amosoisin <szbh.itoshiki.izaya@gmail.com>
2020-03-13 22:18:53 +0900

    Add mul and div and nest function

--- ccc.c
+++ ccc.c
@@ -7,6 +7,42 @@
 #include <string.h>
 
 typedef enum{
+    ND_ADD,
+    ND_SUB,
+    ND_MUL,
+    ND_DIV,
+    ND_NUM,
+}NodeKind;
+
+typedef struct Node Node;
+struct Node {
+    NodeKind kind;  // node type
+    Node *lhs;      // Left Node
+    Node *rhs;      // Right Node
+    int val;        // use if kind is ND_NUM
+};
+
+
+Node *expr(void);
+Node *mul(void);
+Node *primary(void);
+
+Node *new_node(NodeKind kind, Node *lhs, Node *rhs){
+    Node *node = calloc(1, sizeof(Node));
+    node->kind = kind;
+    node->lhs = lhs;
+    node->rhs = rhs;
+    return node;
+}
+
+Node *new_node_num(int32_t val){
+    Node *node = calloc(1, sizeof(Node));
+    node->kind = ND_NUM;
+    node->val = val;
+    return node;
+}
+
+typedef enum{
     TK_RESERVED,    // operator
     TK_NUM,         // integer token
     TK_EOF,         // end of file
@@ -28,8 +64,6 @@ void error_at(char *loc, char *fmt, ...){
     va_list ap;
     va_start(ap, fmt);
 
-    fprintf(stderr, "%d\n", loc);
-    fprintf(stderr, "%d\n", user_input);
     int32_t pos = loc - user_input;
     fprintf(stderr, "%s\n", user_input);
     fprintf(stderr, "%*s", pos, "");
@@ -71,6 +105,45 @@ int32_t expect_number(){
 }
 
 
+// expr = mul ("+" mul | "-" mul)*
+Node *expr(){
+    Node *node = mul();
+
+    while(1){
+        if (consume('+')){
+            node = new_node(ND_ADD, node, mul());
+        }else if (consume('-')){
+            node = new_node(ND_SUB, node, mul());
+        }else{
+            return node;
+        }
+    }
+}
+
+// mul = primary ('*' primary | "/" primary)
+Node *mul(){
+    Node *node = primary();
+    while(1){
+        if (consume('*')){
+            node = new_node(ND_MUL, node, primary());
+        }else if (consume('/')){
+            node = new_node(ND_DIV, node, primary());
+        }else{
+            return node;
+        }
+    }
+}
+
+// primary = num | "(" expr ")"
+Node *primary(){
+    if (consume('(')){
+        Node *node = expr();
+        expect(')');
+        return node;
+    }
+    return new_node_num(expect_number());
+}
+
 bool at_eof(){
     return token->kind == TK_EOF;
 }
@@ -98,7 +171,9 @@ Token *tokenize(char *p){
             continue;
         }
 
-        if (*p == '+' || *p == '-'){
+        if (*p == '+' || *p == '-' 
+                || *p == '*' || *p == '/'
+                || *p == '(' || *p == ')'){
             cur = new_token(TK_RESERVED, cur, p++);
             continue;
         }
@@ -116,6 +191,31 @@ Token *tokenize(char *p){
 }
 
 
+void gen(Node *node){
+    if (node->kind == ND_NUM){
+        printf("    push %d\n", node->val);
+        return;
+    }
+
+    gen(node->lhs);
+    gen(node->rhs);
+
+    printf("    pop rdi\n");
+    printf("    pop rax\n");
+
+    if (node->kind == ND_ADD){
+        printf("    add rax, rdi\n");
+    }else if (node->kind == ND_SUB){
+        printf("    sub rax, rdi\n");
+    }else if (node->kind == ND_MUL){
+        printf("    imul rax, rdi\n");
+    }else if (node->kind == ND_DIV){
+        printf("    cqo\n");
+        printf("    idiv rdi\n");
+    }
+    printf("    push rax\n");
+}
+
 int main(int32_t argc, int8_t **argv){
     if (argc != 2){
         fprintf(stderr, "Invalid argument!!");
@@ -124,23 +224,15 @@ int main(int32_t argc, int8_t **argv){
 
     user_input = argv[1];
     token = tokenize(user_input);
+    Node *node = expr();
 
     printf(".intel_syntax noprefix\n");
     printf(".global main\n");
     printf("main:\n");
 
-    printf("    mov rax, %d\n", expect_number());
-    
-    while (!at_eof()){
-        if (consume('+')) {
-            printf("    add rax, %d\n", expect_number());
-            continue;
-        }
+    gen(node);
 
-        expect('-');
-        printf("    sub rax, %d\n", expect_number());
-    }
-
+    printf("    pop rax\n");
     printf("    ret\n");
 
     return 0;
